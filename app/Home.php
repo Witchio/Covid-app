34eb2fea89390d8e25f15b2fe56f0559fc611164
joundefined <joundefined@users.noreply.github.com>
2020-08-20 15:16:45 +0200

    Wrong Home Controller+Model

---
+++ app/Home.php
@@ -0,0 +1,15 @@
+<?php
+// php artisan make:model Home
+// https://laravel.com/docs/7.x/eloquent#defining-models
+// php artisan make:controller --resource HomeController
+// php artisan make:controller --resource PostController
+// https://laravel.com/docs/7.x/controllers
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Home extends Model
+{
+    //
+}
