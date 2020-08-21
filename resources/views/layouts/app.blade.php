282919a35de93d59ae9c3e761138412ed2587db1
pintoedu <65950405+pintoedu@users.noreply.github.com>
2020-08-21 15:22:39 +0200

    Added navbar left links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,18 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">{{ __('Stats') }}</a>
+                        </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">{{ __('Posts') }}</a>
+                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">{{ __('About Us') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
