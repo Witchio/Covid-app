504b745f6bdea07f55ae5aaad97580bb285afa17
pintoedu <65950405+pintoedu@users.noreply.github.com>
2020-08-21 11:09:32 +0200

    resgiter login and database update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             // $table->timestamp('email_verified_at')->nullable();
-            $table->string('password_hashed');
+            $table->string('password');
             $table->rememberToken();
             $table->enum('role', ['user', 'admin']);
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/posts', 'PostController@index');
 Route::get('/main', function () {
     return view('main');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
