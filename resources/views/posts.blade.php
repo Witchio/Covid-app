368699f381b709520d71546fb93e0363caafe414
luciana-s <65956715+luciana-s@users.noreply.github.com>
2020-08-21 11:41:07 +0200

    fixed db  error

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,12 +5,11 @@
 <h1>Posts</h1>
 @foreach($posts as $post)
 <article>
-
+    <h2>{{ $post->title }}</h2>
+    <p>{{ $post->content }}</p>
+    <img src="{{asset("images/$post->image")}}" alt="post image">
+    <a href="/posts/{{$post->id}}">See more</a>
+    <p>likes</p> <!-- TODO join table query -->
+    <p>comments</p> <!-- TODO join table query -->
+    @endforeach
 </article>
-<h2>{{ $post->title }}</h2>
-<p>{{ $post->content }}</p>
-<img src="{{asset("images/$post->image")}}" alt="post image">
-<a href="/posts/{{$post->id}}">See more</a>
-<p>likes</p> <!-- TODO join table query -->
-<p>comments</p> <!-- TODO join table query -->
-@endforeach
