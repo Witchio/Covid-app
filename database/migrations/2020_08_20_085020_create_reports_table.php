14db5f5ec131de06249d1ffa3cb90e886502a841
Witchio <65949225+Witchio@users.noreply.github.com>
2020-08-25 14:39:11 +0200

    delete posts
    Added the onDelete('cascade') to all the migrations

--- database/migrations/2020_08_20_085020_create_reports_table.php
+++ database/migrations/2020_08_20_085020_create_reports_table.php
@@ -20,8 +20,8 @@ class CreateReportsTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
 
-            $table->foreign('post_id')->references('id')->on('posts');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
