8f90e5f9649255e4324397163e50b314b2d2b2b1
pintoedu <65950405+pintoedu@users.noreply.github.com>
2020-08-25 16:13:46 +0200

    Profile update 2

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,17 +5,16 @@
 @section('content')
 
 <h3>Hellooo <a> {{ Auth::user()->name }}</a></h3>
-<p>Please update some additional info from you</p>
+<p>Please update some additional info</p>
 
 <!-- Profile forms --> 
 <div class="profileForm">
     <form action="#" method="POST">
-        <input type="text" name="firstName" placeholder="First Name">
-        <input type="text" name="lastName" placeholder="Last Name">
+        <input type="text" name="first_name" placeholder="First Name">
+        <input type="text" name="last_name" placeholder="Last Name">
         <input type="text" name="nationality" placeholder="Nationality">
         <input type="text" name="birthdate" placeholder="Date of Birth">
-        <input type="text" name="country" placeholder="Country">
-        <input type="img" name="avatar" placeholder="Choose your avatar">
+        <input type="text" name="country" placeholder="Country">       
         <input type="submit" value="Send">
     </form>
 
