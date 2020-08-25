008e24d414a40c25a0d4a3a1d41138326336909d
Witchio <65949225+Witchio@users.noreply.github.com>
2020-08-25 19:23:02 +0200

    posts bug fix
    the create post button disappeared during the merge with master

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,25 +4,42 @@
 <!-- Looop to display posts -->
 @section('content')
 <h1>Posts</h1>
+
+<!-- when clicked goes to PostController create() -->
+<button id="create">Create post</button>
+<a href="{{ route('post.create') }}">create link</a>
+
+
+<!-- Looop to display posts, getting the data from PostController-->
+
 @foreach($posts as $post)
 <article>
 
     <h2>{{ $post->title }}</h2>
-    <p>{{ $post->content }}</p>
     <img src="{{asset("images/$post->image")}}" alt="post image">
+    <p>{{ @substr($post->content,0,100 ) }} ...</p>
     <a href="/posts/{{$post->id}}">See more</a>
     <p>likes</p> <!-- TODO join table query -->
-    <p>Comments :</p>
-    <ul>
-        @foreach($post->comments as $comment)
-        <li>{{$comment->comment}}</li>
-        @endforeach
-    </ul>
+    <p>comments</p> <!-- TODO join table query -->
+</article>
+<p>Comments :</p>
+<ul>
+    @foreach($post->comments as $comment)
+    <li>{{$comment->comment}}</li>
+    @endforeach
+    <!-- Only show if user is logged is-->
+    @if (Route::has('login'))
+    @auth
     <form action="/posts/edit/{{ $post->id}}" method="post">
         @csrf
         <input type="text" name="comment">
         <input type="submit" value="Comment">
     </form>
-    @endforeach
+    @endauth
+    @endif
 
-    @endsection
+
+</ul>
+@endforeach
+
+@endsection
