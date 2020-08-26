1d238f768f7dfaf05a5cb37535d33faa4c8f29c4
joundefined <joundefined@users.noreply.github.com>
2020-08-26 17:00:03 +0200

    first step add username not working

--- app/Comment.php
+++ app/Comment.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
-
+    // ONE User has MANY Comments (inverse)
+    public function commentor()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
