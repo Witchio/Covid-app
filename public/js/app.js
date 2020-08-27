7c0c716b89090fab0fd545ca26bad97e10f84003
Witchio <65949225+Witchio@users.noreply.github.com>
2020-08-27 15:13:39 +0200

    Admin-users css

--- public/js/app.js
+++ public/js/app.js
@@ -37313,6 +37313,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/admin-users.scss":
+/*!*****************************************!*\
+  !*** ./resources/sass/admin-users.scss ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/sass/app.scss":
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
@@ -37347,16 +37358,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /***/ }),
 
 /***/ 0:
-/*!**********************************************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/nav.scss ./resources/sass/homepage.scss ***!
-  \**********************************************************************************************************************/
+/*!********************************************************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/nav.scss ./resources/sass/homepage.scss ./resources/sass/admin-users.scss ***!
+  \********************************************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\wamp64\www\Project\final_project\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! D:\wamp64\www\Project\final_project\resources\sass\app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! D:\wamp64\www\Project\final_project\resources\sass\nav.scss */"./resources/sass/nav.scss");
-module.exports = __webpack_require__(/*! D:\wamp64\www\Project\final_project\resources\sass\homepage.scss */"./resources/sass/homepage.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/nav.scss */"./resources/sass/nav.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/homepage.scss */"./resources/sass/homepage.scss");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/admin-users.scss */"./resources/sass/admin-users.scss");
 
 
 /***/ })
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require("laravel-mix");
 mix.js("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
     .sass("resources/sass/nav.scss", "public/css")
-    .sass("resources/sass/homepage.scss", "public/css");
+    .sass("resources/sass/homepage.scss", "public/css")
+    .sass("resources/sass/admin-users.scss", "public/css");
