0a2f2713d043988053f06a7cd9f156b506e81166
luciana-s <65956715+luciana-s@users.noreply.github.com>
2020-08-28 11:32:33 +0200

    footer - about.us

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -11,32 +11,32 @@
 <h1>Section about us</h1>
 <section>
 
-    <article id="mitchio">
+    <article id="mitchio" class="dark">
         <img src='{{asset("images/supremeleader.jpg")}}' alt="leader pic">
         <h2>SUPREME LEADER</h2>
         <p>
-            Organa kit sidious owen antilles. Bothan dooku ben palpatine. Moff hutt darth mara lars skywalker moff windu yoda. Darth fisto darth lando lando lando fisto. Darth dantooine moff jabba amidala kessel calrissian. Sidious maul mandalore antilles tatooine luke moff darth. Sith leia darth antilles darth. Lando jade hutt obi-wan mon mothma ackbar. Wicket hutt darth mon wookiee hutt endor tatooine. Skywalker windu darth baba mon. Chewbacca mustafar solo darth wampa maul gamorrean. Chewbacca solo yavin gamorrean ackbar skywalker yoda yoda.</p>
+            Organa kit sidious owen antilles. Bothan dooku ben palpatine. Moff hutt darth mara lars skywalker moff windu yoda. Darth fisto darth lando lando lando fisto. Darth dantooine moff jabba amidala kessel calrissian. Sidious maul mandalore antilles tatooine luke moff darth. Sith leia darth antilles darth. Lando jade hutt obi-wan mon mothma ackbar.</p>
     </article>
     <hr>
 
-    <article id="luchi">¨
+    <article id="luchi" class="light">¨
         <img src=' {{asset("images/luchi.jpg")}}' alt="leader pic">
         <h2>MAIN LEADER</h2>
         <P>Loaded to the gunwalls flogging aye Corsair spike Buccaneer keel furl strike colors yawl. Fathom lee ho Buccaneer lanyard starboard Shiver me timbers smartly dance the hempen jig Yellow Jack. Chase lanyard squiffy pillage Yellow Jack snow execution dock Admiral of the Black Pirate Round run a rig.</P>
     </article>
     <hr>
 
-    <article id="jo">
+    <article id="jo" class="dark">
         <img src='{{asset("images/jo.jpg")}}' alt="leader pic">
         <h2>MISLEADER</h2>
-        <P>Toffee muffin tootsie roll. Biscuit pudding apple pie pie. Chocolate bar bear claw gummies jelly-o chocolate sesame snaps danish macaroon. Chupa chups candy croissant dragée toffee. Pastry cookie wafer cake gummi bears. Jelly-o liquorice sweet roll caramels marzipan gummies gummies wafer biscuit. Tart liquorice cake bear claw cotton candy danish. Tiramisu gummi bears macaroon. Cupcake croissant donut tootsie roll pudding sweet roll bear claw. Tart gummies carrot cake topping cheesecake cake dragée liquorice lollipop. Cotton candy candy soufflé pastry toffee gingerbread. Pudding cake cupcake chocolate. Jujubes croissant apple pie topping powder oat cake. Sweet roll bear claw candy canes.</P>
+        <P>Toffee muffin tootsie roll. Biscuit pudding apple pie pie. Chocolate bar bear claw gummies jelly-o chocolate sesame snaps danish macaroon. Chupa chups candy croissant dragée toffee. Pastry cookie wafer cake gummi bears. Jelly-o liquorice sweet roll caramels marzipan gummies gummies wafer biscuit. Tart liquorice cake bear claw cotton candy danish. Tiramisu gummi bears macaroon.</P>
     </article>
     <hr>
 
-    <article id="edu">
+    <article id="edu" class="light">
         <img src=' {{asset("images/edu.jpg")}}' alt="leader pic">
         <h2>EDU LEADER</h2>
-        <p>We need to harvest synergy effects time to open the kimono that ipo will be a game-changer, and build on a culture of contribution and inclusion. First-order optimal strategies personal development yet future-proof take five, punch the tree, and come back in here with a clear head pulling teeth, optimize the fireball and we need to harvest synergy effects. Let me know if you need me to crack any skulls vertical integration, and tbrand terrorists. Staff engagement. Today shall be a cloudy day, thanks to blue sky thinking, we can now deploy our new ui to the cloud drop-dead date, yet what the yet show pony, nor start procrastinating 2 hours get to do work while procrastinating open book pretend to read while manager stands and watches silently nobody is looking quick do your web search manager caught you and you are fured. Player-coach. Driving the initiative forward workflow ecosystem.</p>
+        <p>We need to harvest synergy effects time to open the kimono that ipo will be a game-changer, and build on a culture of contribution and inclusion. First-order optimal strategies personal development yet future-proof take five, punch the tree, and come back in here with a clear head pulling teeth, optimize the fireball and we need to harvest synergy effects.</p>
     </article>
 
 </section>
