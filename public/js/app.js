1a501a251997ae5c8ade072ee26898b175675ce1
Witchio <65949225+Witchio@users.noreply.github.com>
2020-08-28 11:57:29 +0200

    profile deisgn

--- public/js/app.js
+++ public/js/app.js
@@ -37430,17 +37430,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\final_project\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\wamp64\www\final_project\resources\sass\app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! C:\wamp64\www\final_project\resources\sass\nav.scss */"./resources/sass/nav.scss");
-__webpack_require__(/*! C:\wamp64\www\final_project\resources\sass\homepage.scss */"./resources/sass/homepage.scss");
-__webpack_require__(/*! C:\wamp64\www\final_project\resources\sass\admin-users.scss */"./resources/sass/admin-users.scss");
-__webpack_require__(/*! C:\wamp64\www\final_project\resources\sass\admin-posts.scss */"./resources/sass/admin-posts.scss");
-__webpack_require__(/*! C:\wamp64\www\final_project\resources\sass\stats.scss */"./resources/sass/stats.scss");
-__webpack_require__(/*! C:\wamp64\www\final_project\resources\sass\profile.scss */"./resources/sass/profile.scss");
-__webpack_require__(/*! C:\wamp64\www\final_project\resources\sass\posts.scss */"./resources/sass/posts.scss");
-__webpack_require__(/*! C:\wamp64\www\final_project\resources\sass\add-post.scss */"./resources/sass/add-post.scss");
-module.exports = __webpack_require__(/*! C:\wamp64\www\final_project\resources\sass\post.scss */"./resources/sass/post.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/nav.scss */"./resources/sass/nav.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/homepage.scss */"./resources/sass/homepage.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/admin-users.scss */"./resources/sass/admin-users.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/admin-posts.scss */"./resources/sass/admin-posts.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/stats.scss */"./resources/sass/stats.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/profile.scss */"./resources/sass/profile.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/posts.scss */"./resources/sass/posts.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/add-post.scss */"./resources/sass/add-post.scss");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/final_project/resources/sass/post.scss */"./resources/sass/post.scss");
 
 
 /***/ })
