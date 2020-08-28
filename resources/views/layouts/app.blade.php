0bc4349ee141f4b173cab286c98b4ea2970dbad7
Witchio <65949225+Witchio@users.noreply.github.com>
2020-08-28 12:09:50 +0200

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     <link href="{{ asset('css/nav.css') }}" rel="stylesheet">
 
     <!-- Font Awesome CDN -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" /> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
 
     <!-- to do a separate stylesheet than the nav one (with general css), if needed -->
     @yield('style')
@@ -121,14 +121,14 @@
 
         <div class="footer-left">
 
-        <p class="footer-links">
-            <a class="link-1" href="{{ route('main') }}">{{ __('Home') }}</a>
-            <a class="link-2" href="{{ route('stats') }}">{{ __('Stats') }}</a>
-            <a class="link-3" href="{{ route('posts') }}">{{ __('Posts') }}</a>
-            <a class="link-4" href="{{ route('about-us') }}">{{ __('About') }}</a>
-        </p>
+            <p class="footer-links">
+                <a class="link-1" href="{{ route('main') }}">{{ __('Home') }}</a>
+                <a class="link-2" href="{{ route('stats') }}">{{ __('Stats') }}</a>
+                <a class="link-3" href="{{ route('posts') }}">{{ __('Posts') }}</a>
+                <a class="link-4" href="{{ route('about-us') }}">{{ __('About') }}</a>
+            </p>
 
-        <p>Cov-Aid 19 &copy; 2020</p>
+            <p>Cov-Aid 19 &copy; 2020</p>
 
         </div>
 
