a8220d08438b61110f46cf3698fbe7ab3c82cc32
Witchio <65949225+Witchio@users.noreply.github.com>
2020-08-28 12:36:27 +0200

    Revert "profile design"
    This reverts commit a0d04af2ae89b937f3a8538a622d45aa74fe892d, reversing
    changes made to 9f48b321b33655f2209d1e72b91820bac474d894.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,28 +9,28 @@
 
 <!-- <h3>Hellooo <a> {{ Auth::user()->name }}</a></h3>
 <h4>Your email is <a> {{ Auth::user()->email }}</a></h4> -->
-<h1 id="title">Modify personal data</h1>
+<h1>Modify personal data</h1>
 
 <!-- Profile forms -->
 <div class="profileForm">
     <form action="/profile/update" method="post">
         @csrf
         @method('PUT')
-        <h4>First Name</h4>
+        <label for="">First Name</label>
         <input type="text" class="form-control" name="first_name" placeholder="First Name" value="{{ Auth::user()->first_name }}">
-        <h4>Last Name</h4>
+        <label for="">Last Name</label>
         <input type="text" class="form-control" name="last_name" placeholder="Last Name" value="{{ Auth::user()->last_name }}">
         <!-- <input type="text" name="nationality" placeholder="Nationality" value="{{ Auth::user()->nationality }}"> -->
-        <h4>Nationality</h4>
+        <label for="">Nationality</label>
         <select class="form-control" id="select" name="nationality">
             <option selected>{{ Auth::user()->nationality }}</option>
             <option class="option" value="">...</option>
         </select>
-        <h4>Birthdate</h4>
+        <label for="">Birthdate</label>
         <input type="date" class="form-control" name="birthdate" placeholder="Date of Birth" value="{{ Auth::user()->birthdate }}">
-        <h4>Country</h4>
+        <label for="">Country</label>
         <input type="text" class="form-control" name="country" placeholder="Country" value="{{ Auth::user()->country }}">
-        <input type="submit" class="btn btn-primary" value="Edit">
+        <input type="submit" value="Edit">
     </form>
 
 </div>
