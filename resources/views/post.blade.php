371e72678157c81c8c47fbfa01b3633aab2568ca
joundefined <joundefined@users.noreply.github.com>
2020-08-28 13:22:36 +0200

    delete/edit btns.post main sxn layout

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,110 +9,119 @@
 @section('content')
 
 <!-- individual post depending on id -->
-<h2>{{ $posts[0]->title }}</h2>
-<img src="" alt="">
-@if($posts[0]->image)
-<img src="{{asset("images/$img")}}" alt="post image">
-@endif
-<p>{{ $posts[0]->content }}</p>
+<h1>{{ $posts[0]->title }}</h1>
+<section class="maincontainer">
+    <img src="" alt="">
+    @if($posts[0]->image)
+    <img src="{{asset("images/$img")}}" alt="post image">
+    @endif
+
+    <p>{{ $posts[0]->content }}</p>
 
 
-<!-- Only show if user is logged in-->
-@if (Auth::user())
-<p>{{$posts[0]->user_id}}
-    <p>
-        <p>Hello</p>
+    <section class="authoraccess">
+        <!-- {{$posts[0]->user_id}} -->
+        <!-- Only show if user is logged in-->
+        @if (Auth::user())
         <!-- only for the AUTHOR -->
         @auth
         @if ($posts[0]->user_id==Auth::user()->id)
-        <p>
-            <a href="/post/update/{{$posts[0]->id}}">Edit post details</a>
-        </p>
+        <br>
+        <div id="editfakebtn">
+            <a href="/post/update/{{$posts[0]->id}}" id="editbg">Edit post details</a>
+        </div>
         @endif
         @endauth
-        @endif
-        <br>
-
-        <div>{{ $posts[0]->users_count }} likes</div>
-
-        <!-- Only show if user is logged is-->
-        @if (Route::has('login'))
-        @auth
-
-        <!-- Should be icon probably -->
-        <button class="like-btn" @if ($liked) style="display: none;" @endif value="{{$posts[0]->id}}" onclick="toggleLike()"><i class="far fa-thumbs-up"></i></button>
-        <button class="like-btn" @if (!$liked) style="display: none;" @endif value="{{$posts[0]->id}}" onclick="toggleLike()"><i class="fas fa-thumbs-up"></i></button>
-
-        <!-- IF user has not reported this post before -->
-        @if(!$reported)
-        <!-- IF user is not the post author -->
-        @if($posts[0]->user_id!=Auth::user()->id)
-        <a href="/post/report/{{$posts[0]->id}}"><button id="report"><i class="far fa-flag"></i></button></a>
-        @endif
-        @endif
         @endif
 
         @if(Auth::user() !== null)
         @if($posts[0]->user_id==Auth::user()->id ||Auth::user()->role == "admin")
         <!-- If user that created the post or admin wants to permanently delete it-->
-        <a href="/post/delete/{{$posts[0]->id}}"><button id="report">Delete</button></a>
-
-        @endauth
+        <div>
+            <a href="/post/delete/{{$posts[0]->id}}"><button id="delete">Delete Post</button></a>
+        </div>
         @endif
         @endif
+    </section>
+    <br>
 
+    <section class="maininfo">
+        <span>{{ $posts[0]->users_count }} likes</span>
 
-        <br><br>
-        <p>-----Comments ({{ $posts[0]->comments_count }})-----</p>
-        <ul>
-            @foreach($posts as $post)
-            <li>{{$post->comment}}</li>
-            <li>{{$post->name}}</li>
-            @endforeach
-        </ul>
-
-
-        <!-- Comment post-->
+        <!-- Only show if user is logged is-->
         @if (Route::has('login'))
         @auth
-        <form action="/posts/comment/{{ $posts[0]->id}}" method="post">
-            @csrf
-            <input type="text" name="comment">
-            <input type="submit" value="Comment">
-        </form>
-        @endauth
+        <button class="like-btn" @if ($liked) style="display: none;" @endif value="{{$posts[0]->id}}" onclick="toggleLike()"><i class="far fa-thumbs-up"></i></button>
+        <button class="like-btn" @if (!$liked) style="display: none;" @endif value="{{$posts[0]->id}}" onclick="toggleLike()"><i class="fas fa-thumbs-up"></i></button>
+        <!-- IF user has not reported this post before -->
+        @if(!$reported)
+        <!-- IF user is not the post author -->
+        @if($posts[0]->user_id!=Auth::user()->id)
+        <button id="report"><a href="/post/report/{{$posts[0]->id}}"><i class="far fa-flag"></i></a></button>
+        @endif
         @endif
 
-        @endsection
+
+
+        @endauth
+        @endif
+    </section>
+</section>
+
+
+<br><br>
+<section class="commentscontainer">
+    <p>-----Comments ({{ $posts[0]->comments_count }})-----</p>
+    <ul>
+        @foreach($posts as $post)
+        <li>{{$post->comment}}</li>
+        <li>{{$post->name}}</li>
+        @endforeach
+    </ul>
+
+
+    <!-- Comment post-->
+    @if (Route::has('login'))
+    @auth
+    <form action="/posts/comment/{{ $posts[0]->id}}" method="post">
+        @csrf
+        <input type="text" name="comment">
+        <input type="submit" value="Comment">
+    </form>
+    @endauth
+    @endif
+</section>
+
+@endsection
 
 
 
-        @section('js-resources')
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+@section('js-resources')
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
-        <script src="{{ asset('js/post.js') }}"></script>
+<script src="{{ asset('js/post.js') }}"></script>
 
-        <script>
-            $(function() {
-                $('.like-btn').click(function(e) {
-                    let route = '/post/like/' + $(this).val();
-                    console.log('Route: ' + route);
-                    $.ajax({
-                        url: route,
-                        type: 'get',
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                        },
-                        success: function(result) {
-                            console.log(result.message);
-                            $('.content').load(document.URL + ' .content');
-                        },
-                        error: function(err) {
-                            // If ajax errors happens
-                            alert('Please Log In or Register to LIKE posts : AJAX ERROR.');
-                        }
-                    });
-                });
+<script>
+    $(function() {
+        $('.like-btn').click(function(e) {
+            let route = '/post/like/' + $(this).val();
+            console.log('Route: ' + route);
+            $.ajax({
+                url: route,
+                type: 'get',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                success: function(result) {
+                    console.log(result.message);
+                    $('.content').load(document.URL + ' .content');
+                },
+                error: function(err) {
+                    // If ajax errors happens
+                    alert('Please Log In or Register to LIKE posts : AJAX ERROR.');
+                }
             });
-        </script>
-        @endsection
+        });
+    });
+</script>
+@endsection
