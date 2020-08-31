f18377a1529761e3d6bcfea67aee752c1fc8f84f
Witchio <65949225+Witchio@users.noreply.github.com>
2020-08-31 10:52:12 +0200

    password reset css

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
+@section('style')
+<link href="{{ asset('css/password-reset.css') }}" rel="stylesheet">
+@endsection
+
+<h1>hello</h1>
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -20,9 +25,9 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -34,9 +39,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
