d81c67ff1af064145cedef884f2a6ddbca3e90dc
Witchio <65949225+Witchio@users.noreply.github.com>
2020-09-01 09:52:18 +0200

    comment fix

--- resources/views/admin-posts.blade.php
+++ resources/views/admin-posts.blade.php
@@ -59,7 +59,7 @@
         @endforeach
     </section>
 
-    <!-- Ajax call to delete post -->
+    <!-- Ajax call to restore post -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script>
         $(function() {
