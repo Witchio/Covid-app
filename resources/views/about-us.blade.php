4c1fb04b8da93f59d2294d5ae1505a9c9a928183
luciana-s <65956715+luciana-s@users.noreply.github.com>
2020-09-01 11:10:12 +0200

    about-us luchi and jo

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -8,7 +8,7 @@
 
 <!-- Content -->
 @section('content')
-<h1>About us/ Contact</h1>
+<h1>About us / Contact</h1>
 <section id="cards">
 
     <article id="mitchio" class="dark">
@@ -28,7 +28,7 @@
         <div class="content">
             <img src=' {{asset("images/luciana.jpg")}}' alt="leader pic">
             <h2>Luciana Scalfaro</h2>
-            <P>Loaded to the gunwalls flogging aye Corsair spike Buccaneer keel furl strike colors yawl. Fathom lee ho Buccaneer lanyard starboard Shiver me timbers smartly dance the hempen jig Yellow Jack. Chase lanyard squiffy pillage Yellow Jack snow execution dock Admiral of the Black Pirate Round run a rig.</P>
+            <P>Hello, my name is Luciana, and i am from Argentina. I like piña coladas and getting caught in the rain. On the project i worked on various posts features, and on Responsive Web Design. I was the point person for conflicts and issues on GitHub, our collaboration platform. I helped design the website layout and writing the code that connects our site to our database. Write to me and escape</P>
         </div>
         <div class="links">
             <a href="https://www.linkedin.com/in/luciana-scalfaro/" target="_blank"><i class="fab fa-linkedin"></i></a>
@@ -40,7 +40,7 @@
         <div class="content">
             <img src='{{asset("images/joanna.jpg")}}' alt="leader pic">
             <h2>Joanna Wong </h2>
-            <p>@joundefined likes to dream about rainbows and unicorns and being pretty. She does not spend time honing her coding skills. She doesn't enjoy solving logical or technical puzzles. She doesn't live and breathe code and algorithms. SHe doesn't aspire to be part of the underground hacker community, because that doesn't exsist, obviously.</p>
+            <p>Hello world! I oversaw the database design and execution process. I helped write the code that connects our website to our Database and added various security features on different levels of the data structure to the website to manage Access Permissions to our features and overall Data Security. I also worked on the website layout design and implemented Responsive Web Design for multi-device usage.</p>
         </div>
         <div class="links">
             <a href="https://www.linkedin.com/in/joanna-w-4584a9163/" target="_blank"><i class="fab fa-linkedin"></i></a>
@@ -52,7 +52,7 @@
         <div class="content">
             <img src=' {{asset("images/eduardo.jpg")}}' alt="leader pic">
             <h2>Eduardo Pinto</h2>
-             <p>Hi, I'm Eduardo. My main focus in the project was to define a way to include a responsive news feed to the main page which displays the most recent events about Covid-19 pandemic. Then I've created a profile page, that gets and sends user info into the database, and improved the original navigation bar with some new links and routes. Finally I created a responsive footer with social networking interaction.</p>
+            <p>Hi, I'm Eduardo. My main focus in the project was to define a way to include a responsive news feed to the main page which displays the most recent events about Covid-19 pandemic. Then I've created a profile page, that gets and sends user info into the database, and improved the original navigation bar with some new links and routes. Finally I created a responsive footer with social networking interaction.</p>
         </div>
 
         <div class="links">
