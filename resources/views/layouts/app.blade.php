5033c9b8bf19b96fe6fe220b2a983fa7a39cba48
Witchio <65949225+Witchio@users.noreply.github.com>
2020-09-01 13:03:51 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Cov-aid</title>
+    <title>Cov-aid 19</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
